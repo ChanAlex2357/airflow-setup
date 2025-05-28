3ba950dc002c018e87832b073c326854b4de1131
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-28 14:23:07 +0300

    initiate transfer file for tasks

---
+++ dags/__pycache__/transfer.py
@@ -0,0 +1,6 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.task_group import TaskGroup
+from datetime import datetime
